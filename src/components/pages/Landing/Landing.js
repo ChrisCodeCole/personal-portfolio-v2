4645075fc3d668d7dc1c232a11beffade5278611
Christian Coleman <christian.c2508@gmail.com>
2020-02-28 00:43:35 -0500

    initial translate animations

--- src/components/pages/Landing/Landing.js
+++ src/components/pages/Landing/Landing.js
@@ -1,20 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import classes from './Landing.module.css';
+import { useSpring, animated } from 'react-spring';
 
 export default function Landing() {
+	const [testBool, setTestBool] = useState(true);
+
+	const topContainerProps = useSpring({
+		config: {
+			duration: 1000,
+		},
+		delay: 100,
+		transform: 'translateY(0%)',
+		opacity: 1,
+		from: {
+			opacity: 0,
+			transform: 'translateY(-100%)',
+		},
+	});
+
+	const bottomContainerProps = useSpring({
+		config: {
+			duration: 1000,
+		},
+		delay: 100,
+		transform: 'translateY(0%)',
+		opacity: 1,
+		from: {
+			opacity: 0,
+			transform: 'translateY(100%)',
+		},
+		onRest: () => {
+			console.log(testBool);
+			setTestBool(false);
+		},
+	});
+
+	useEffect(() => {
+		console.log(testBool);
+	}, [testBool]);
+
 	return (
 		<div className={classes.landingWrapper}>
-			<h1 className={classes.nameHeader}>Christian Coleman</h1>
-			<span className={classes.headerSeparator} />
-			<h3 className={classes.subHeader}> Front End Developer</h3>
-			<div className={classes.bottomContainer}>
+			<animated.div
+				style={topContainerProps}
+				className={classes.topContainer}
+			>
+				<h1 className={classes.nameHeader}>Christian Coleman</h1>
+				<span className={classes.headerSeparator} />
+				<h3 className={classes.subHeader}> Front End Developer</h3>
+			</animated.div>
+			<animated.div
+				style={bottomContainerProps}
+				className={classes.bottomContainer}
+			>
 				<span className={classes.scrollText}>SCROLL</span>
 				<div className={classes.bubblesWrapper}>
 					<span className={classes.bubbles} />
 					<span className={classes.bubbles} />
 					<span className={classes.bubbles} />
 				</div>
-			</div>
+			</animated.div>
 		</div>
 	);
 }
