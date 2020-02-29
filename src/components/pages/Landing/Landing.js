6cb527f2f264fd5aa339b50c3e8e156272377685
Christian Coleman <christian.c2508@gmail.com>
2020-02-28 23:56:24 -0500

    testing out animations

--- src/components/pages/Landing/Landing.js
+++ src/components/pages/Landing/Landing.js
@@ -1,9 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import classes from './Landing.module.css';
-import { useSpring, animated } from 'react-spring';
+import { easeSinInOut } from 'd3-ease';
+import { animated, config, useSpring, useTrail } from 'react-spring';
+
+const numArray = [0, 1, 2];
 
 export default function Landing() {
-	const [testBool, setTestBool] = useState(true);
+	const bubbleSpring = useSpring({
+		config: {
+			...config.gentle,
+			// easing: t => easeSinInOut(t),
+			duration: 3000,
+		},
+		from: {
+			y: 0,
+		},
+		to: async next => {
+			while (1) {
+				console.log('updating y');
+				await next({ y: 1 });
+			}
+		},
+		reset: true,
+	});
+	console.log(bubbleSpring);
+
+	// useEffect(() => {
+
+	// }, []);
 
 	const topContainerProps = useSpring({
 		config: {
@@ -30,15 +54,21 @@ export default function Landing() {
 			transform: 'translateY(100%)',
 		},
 		onRest: () => {
-			console.log(testBool);
-			setTestBool(false);
+			// console.log(testBool);
+			// setTestBool(false);
 		},
 	});
 
-	useEffect(() => {
-		console.log(testBool);
-	}, [testBool]);
+	// useEffect(() => {
+	// 	console.log(testBool);
+	// }, [testBool]);
 
+	console.log(
+		bubbleSpring.y.interpolate({
+			range: [0, 0.25, 0.5, 0.75, 1],
+			output: [0, -30, 0, 30, 0],
+		})
+	);
 	return (
 		<div className={classes.landingWrapper}>
 			<animated.div
@@ -55,9 +85,88 @@ export default function Landing() {
 			>
 				<span className={classes.scrollText}>SCROLL</span>
 				<div className={classes.bubblesWrapper}>
-					<span className={classes.bubbles} />
-					<span className={classes.bubbles} />
-					<span className={classes.bubbles} />
+					{/* {bubbleTrail.map(({ y, ...rest }, index) => (
+						<animated.span
+							key={index}
+							className={classes.bubbles}
+							style={{
+								...rest,
+								transform: y
+									.interpolate({
+										range: [0, 0.25, 0.5, 0.75, 1],
+										output: [0, -30, 0, 30, 0],
+									})
+									.interpolate(y => {
+										// console.log('y', y);
+										return `translateY(${y}%)`;
+									}),
+								// range: [0, 0.2, 0.4, 0.6, 0.8, 1],
+								// output: [0, -30, 0, 30, 0, -30],
+							}}
+						/>
+					))} */}
+					{numArray.map((num, idx) => {
+						return (
+							<span
+								key={num}
+								className={classes.bubbles}
+								style={{
+									...bubbleSpring,
+									transform: bubbleSpring.y
+										.interpolate({
+											range: [0, 0.25, 0.5, 0.75, 1],
+											output: [0, -30, 0, 30, 0],
+										})
+										.interpolate(y => {
+											return `translateY(${y}%)`;
+										}),
+								}}
+							/>
+						);
+					})}
+					{/* <span
+						className={classes.bubbles}
+						style={{
+							...bubbleSpring,
+							transform: bubbleSpring.y
+								.interpolate({
+									range: [0, 0.25, 0.5, 0.75, 1],
+									output: [0, -30, 0, 30, 0],
+								})
+								.interpolate(y => {
+									return `translateY(${y}%)`;
+								}),
+						}}
+					/>
+					<span
+						className={classes.bubbles}
+						style={{
+							...bubbleSpring,
+							transform: bubbleSpring.y
+								.interpolate({
+									range: [0, 0.25, 0.5, 0.75, 1],
+									output: [0, -30, 0, 30, 0],
+								})
+								.interpolate(y => {
+									console.log('interpolating');
+									return `translateY(${y}%)`;
+								}),
+						}}
+					/>
+					<span
+						className={classes.bubbles}
+						style={{
+							...bubbleSpring,
+							transform: bubbleSpring.y
+								.interpolate({
+									range: [0, 0.25, 0.5, 0.75, 1],
+									output: [0, -30, 0, 30, 0],
+								})
+								.interpolate(y => {
+									return `translateY(${y}%)`;
+								}),
+						}}
+					/> */}
 				</div>
 			</animated.div>
 		</div>
